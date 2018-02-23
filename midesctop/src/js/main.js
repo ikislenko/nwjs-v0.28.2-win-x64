c77634a4bb533ad5260e43122122cd9afe53cca4
Ivan Kyslenko <sg4asite@gmail.com>
2018-02-23 17:22:27 +0200

    Add collider

--- midesctop/src/js/main.js
+++ midesctop/src/js/main.js
@@ -39,6 +39,7 @@ function draw() {
     Context.context.fillStyle = "#000";
     Context.context.fillRect(0, 0, Context.canvas.width, Context.canvas.height);
 
+    drawMatrix(arena, { x: 0, y: 0 });
     drawMatrix(Player.matrix, Player.pos);
 }
 
@@ -51,13 +52,62 @@ function update(time = 0) {
     lastTime = time;
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        Player.pos.y++;
-        dropCounter = 0;
+        playerDrop();
     }
     draw();
     requestAnimationFrame(update);
 }
 
+function playerDrop() {
+    Player.pos.y++;
+    if (collide(arena, Player)) {
+        Player.pos.y--;
+        merge(arena, Player);
+        Player.pos.y = 0;
+    }
+    dropCounter = 0;
+}
+
+function createMatrix(w, h) {
+    const matrix = [];
+
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos]
+
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function playerMove(dir) {
+    Player.pos.x += dir;
+    if (collide(arena, Player)) {
+        Player.pos.x -= dir;
+    }
+}
+
+const arena = createMatrix(12, 20);
 
 (function() {
     // Initialize
@@ -66,24 +116,13 @@ function update(time = 0) {
     Context.context.scale(20, 20);
 
     update();
-
     document.addEventListener("keydown", event => {
-        console.log(event)
-
         if (event.keyCode == 37) {
-            Player.pos.x--;
+            playerMove(-1);
+        } else if (event.keyCode == 39) {
+            playerMove(1);
+        } else if (event.keyCode == 40) {
+            playerDrop();
         }
-
-        if (event.keyCode == 39) {
-            Player.pos.x++;
-        }
-
-        // if (event.keyCode == 37) {
-
-        // }
-
-        // if (event.keyCode == 37) {
-
-        // }
     });
 })();
